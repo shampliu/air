1cfc9e643a468db5289ca883209c3ab1e1a0c12f
Chang Liu <shampliu@gmail.com>
2016-08-20 17:13:59 -0700

    click out of menu + font changing color

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,36 @@
 $(document).ready(function() {
   var open = false;
-  $('button.lines-button.x2').on('click', function(e){
+  $('button.lines-button.x2').on('click', function(e) {
     e.preventDefault();
     if(!open){
       $('.main-menu').fadeIn(800);
+      $('body').addClass('noscroll');
       $(this).addClass('close');
+      $('.logo').addClass('menu-logo');
       open = true;
     }
     else{
       $(this).removeClass('close');
+      $('body').removeClass('noscroll');
       $('.main-menu').fadeOut(800);
+      $('.logo').removeClass('menu-logo');
       open = false;
     }
   });
+
+  $('.menu-back').on('click', function(e) {
+    e.preventDefault(); 
+    if (open) {
+      $('button.lines-button.x2').removeClass('close');
+      $('body').removeClass('noscroll');
+      $('.main-menu').fadeOut(800);
+      $('.logo').removeClass('menu-logo');
+      open = false;
+
+    }
+  });
+
+
 
 
 
